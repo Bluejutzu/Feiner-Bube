53993bc7c76315c1527d35dea1fae4b09cf4969e
bluejutzu <kalebdaniel98@gmail.com>
2025-04-13 17:50:50 +0200

    y

--- src/events/ready/ready.ts
+++ src/events/ready/ready.ts
@@ -1,6 +1,7 @@
-import type { Client } from 'discord.js';
-import type { CommandKit } from 'commandkit';
+import type { Client } from "discord.js";
+import type { CommandKit } from "commandkit";
 
-export default function (c: Client<true>, handler: CommandKit) {
-  console.log(`${c.user.username} is ready!`);
-};
+export default function (c: Client<true>) {
+    console.log(`${c.user.username} is ready!`);
+    console.log(`${c.user.username} registered on ${c.guilds.cache.size} servers!`);
+}
